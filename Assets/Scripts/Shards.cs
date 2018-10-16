4bbd460213644c05604ca948adab3da260f64c88
Shawn Clark <sic1278@rit.edu>
2018-10-15 20:13:12 -0400

    Shard and barrier code complete

---
+++ Assets/Scripts/Shards.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shards : MonoBehaviour
+{
+
+    public List<GameObject> shardList;
+    public bool complete;
+    public int numCollected;
+    public List<GameObject> barriers;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject shard in GameObject.FindGameObjectsWithTag("shard"))
+        {
+            shardList.Add(shard);
+        }
+        foreach (GameObject barrier in GameObject.FindGameObjectsWithTag("barrier"))
+        {
+            barriers.Add(barrier);
+        }
+        complete = false;
+        numCollected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numCollected < shardList.Count)
+        {
+            for (int i = 0; i < shardList.Count; i++)
+            {
+                if (shardList[i] != null && shardList[i].GetComponent<MinorShard>().collected)
+                {
+                    numCollected++;
+                    Destroy(shardList[i]);
+                }
+            }
+        }
+        else
+        {
+            complete = true;
+            foreach (GameObject barrier in GameObject.FindGameObjectsWithTag("barrier"))
+            {
+                barrier.GetComponent<Renderer>().enabled = false;
+                barrier.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+
+    }
+}
