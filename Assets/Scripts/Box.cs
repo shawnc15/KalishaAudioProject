3e7bd5287a58b7535289ab76df95948b9bfd829f
Sam Belisle <spb6892@rit.edu>
2018-10-22 10:50:35 -0400

    Basic changes to the box and wall prefabs, added a tag to wall

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,34 +4,59 @@ using UnityEngine;
 
 public class Box : MonoBehaviour {
 
-
+    public Vector3 velocity;
+    public bool collide;
     Rigidbody2D boxBody;
 	// Use this for initialization
 	void Start () {
         boxBody = GetComponent<Rigidbody2D>();
+        collide = false;
+        velocity = boxBody.velocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Constrain();
-	}
+        velocity = boxBody.velocity;
+    }
 
     void Constrain()
     {
        
-            if (boxBody.velocity.magnitude >0)
-            {
-                Vector3 tempVelocity = boxBody.velocity;
+        if (boxBody.velocity.magnitude > 0)
+        {
+            Vector3 tempVelocity = boxBody.velocity;
 
-                if (boxBody.velocity.x>boxBody.velocity.y)
-                {
-                    tempVelocity.y = 0;
-                }
-                else
-                {
-                    tempVelocity.x = 0;
-                }
+            if (boxBody.velocity.x > boxBody.velocity.y)
+            {
+                tempVelocity.y = 0;
+            }
+            else
+            {
+                tempVelocity.x = 0;
             }
         }
-   
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "box")
+        {
+            boxBody.mass = 10.0f;
+        }
+        if (collision.collider.tag == "wall")
+        {
+            
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "box")
+        {
+            boxBody.mass = 0.01f;
+        }
+    }
+
 }
