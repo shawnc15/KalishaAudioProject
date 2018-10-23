9a4a99d99db109339ccc0d6beff612191fd5a8cc
Gilexarr <tmenichel@gmail.com>
2018-10-23 00:10:46 -0400

    Started Dreadtalon

---
+++ Assets/Dreadtalon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dreadtalon : MonoBehaviour {
+
+    // initialize attributes
+    private Scene loadedLevel;
+    public GameObject target;
+    Vector3 acceleration;
+    Vector3 direction;
+    Vector3 desiredVelocity;
+    Vector3 currentVelocity;
+    Vector3 curPos;
+    float maxSpeed;
+    public float accelRate;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
