f8c6865bc2e6c9ce6a784464dade3bfe5f98b218
Gilexarr <tmenichel@gmail.com>
2018-10-23 19:58:39 -0400

    animations done

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class Move: MonoBehaviour {
 
     public float moveRate = .11f;
-
+    public Animator animator;
     public Rigidbody2D playerBody;
-	// Use this for initialization
-	void Start () {
+    Vector3 moveVector = Vector3.zero;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         MovementInput();
-	}
+        animator.SetFloat("xSpeed", Mathf.Abs(moveVector.x));
+        animator.SetFloat("ySpeed", moveVector.y);
+        Debug.Log(Mathf.Abs(moveVector.x));
+    }
 
     private void MovementInput()
     {
-        Vector3 moveVector = Vector3.zero;
+        moveVector = Vector3.zero;
         if (Input.GetAxis("moveUp") > 0)
         {
             moveVector += Vector3.up;
+            
         }
         else if (Input.GetAxis("moveUp") < 0)
         {
@@ -32,11 +37,12 @@ public class Move: MonoBehaviour {
         if (Input.GetAxis("moveRight") > 0)
         {
             moveVector += Vector3.right;
-
+            GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (Input.GetAxis("moveRight") < 0)
         {
             moveVector += Vector3.left;
+            GetComponent<SpriteRenderer>().flipX = true;
         }
 
         moveVector = moveVector.normalized * moveRate;
