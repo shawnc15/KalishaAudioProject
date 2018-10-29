22fd12348dc6d2afb8f3232827bb86d256a8e6b3
Shawn Clark (RIT Student) <sic1278@ad.rit.edu>
2018-10-29 10:52:25 -0400

    Cinderfiend
    Not done yet

--- Assets/Scripts/CinderFiend.cs
+++ Assets/Scripts/CinderFiend.cs
@@ -3,48 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TalonScript : MonoBehaviour {
+public class CinderFiend : MonoBehaviour
+{
 
     public GameObject target;
     public float speed;
     private Vector2 velocity;
     private float distance;
     private Scene loadedLevel;
-    public Rigidbody2D birdBody;
+    public Rigidbody2D fiendBody;
+    private bool startExplode;
     Vector2 moveVector = Vector3.zero;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         loadedLevel = SceneManager.GetActiveScene();
-        birdBody = this.GetComponent<Rigidbody2D>();
+        fiendBody = this.GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("player");
+        startExplode = false;
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        Seek();
-	}
+        if (!startExplode)
+            Seek();
+    }
 
     void Seek()
     {
-        velocity = new Vector2((target.transform.position.x - transform.position.x), (target.transform.position.y - transform.position.y ));
+        velocity = new Vector2((target.transform.position.x - transform.position.x), (target.transform.position.y - transform.position.y));
         moveVector = velocity.normalized * speed;
-        birdBody.velocity = moveVector;
+        fiendBody.velocity = moveVector;
         transform.forward = velocity.normalized;
-        Debug.Log(moveVector);
+    }
+
+    void Explode()
+    {
+        StartCoroutine(Wait());
+        Debug.Log("boom");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "player")
         {
-            SceneManager.LoadScene(loadedLevel.buildIndex);
+            //SceneManager.LoadScene(loadedLevel.buildIndex);
+            startExplode = true;
+            Explode();
+
         }
         if (collision.gameObject.tag == "box")
         {
-            
+
         }
     }
 }
