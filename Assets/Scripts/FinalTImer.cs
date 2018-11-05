48ea5b96120f810954a48f1b9dfc2af866228b72
Sam Belisle <spb6892@rit.edu>
2018-11-04 22:40:02 -0500

    Finished Most Things

---
+++ Assets/Scripts/FinalTImer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalTImer : MonoBehaviour {
+
+
+    public int interval;
+    private int timer;
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timer == interval)
+            SceneManager.LoadScene("Title");
+        timer++;
+
+	}
+}
